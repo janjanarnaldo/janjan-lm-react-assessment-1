e5990162c6b181ca069df8d613da8fe2014e959d
Arjan <janjanarnaldo@gmail.com>
2020-02-14 12:16:11 +0800

    Setup

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Arjan Pogi</div>
+};
+
+export default App;
---
+++ src/components/FlagCard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FlagCard = () => {
+  return <div>FlagCard</div>;
+}
+
+export default FlagCard;
