d438ee333aacf7fc48746f72b4125eba598252b1
Arjan <janjanarnaldo@gmail.com>
2020-02-14 12:36:43 +0800

    Added fake users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import faker from 'faker';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const newUsers = [];
+    while (newUsers.length < 10) {
+      const user = {
+        email: faker.internet.email(),
+        jobTitle: faker.name.jobTitle(),
+        avatar: faker.internet.avatar(),
+        content: faker.lorem.paragraph(),
+        countryCode: faker.address.countryCode(),
+      }
+      newUsers.push(user);
+    }
+
+    setUsers(newUsers);
+  }, []);
+
+  console.log(users, 'users');
+
   return <div>Arjan Pogi</div>
 };
 
